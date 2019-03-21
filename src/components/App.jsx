b60f4e4386d91233d55859a6aabc1e0dec4d83d6
Rodrigo Barreto <rcabarreto@gmail.com>
2019-03-21 11:16:52 -0300

    removed tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
-import Navigation from 'components/Navigation'
-
 export default () => (
   <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-    <Navigation />
     <main role="main" className="inner cover">
       <h1 className="cover-heading">My React Boilerplate</h1>
-      <p className="lead">This</p>
+      <p className="lead">Go!</p>
 
     </main>
   </div>
